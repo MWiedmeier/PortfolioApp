e3316de6a67de5cae71250e5c627e22e78ca5a98
MWiedmeier <mattwiedmeier@gmail.com>
2016-08-19 05:50:12 -0400

    Testing to ensure git set up properly.

--- app/src/main/java/com/example/matt/portfolioapp/MainActivity.java
+++ app/src/main/java/com/example/matt/portfolioapp/MainActivity.java
@@ -7,6 +7,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        System.out.println("Testing git configuration");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
